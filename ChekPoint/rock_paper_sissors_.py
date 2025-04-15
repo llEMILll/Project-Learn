80c2479d72bc9a64817f3d97ab803494ccd47c48
llEMILll <emilwillian2000@gmail.com>
2025-04-15 13:51:45 -0500

    Checkpoint Project

--- ChekPoint/rock_paper_sissors_.py
+++ ChekPoint/rock_paper_sissors_.py
@@ -1,5 +1,6 @@
+#random module
 from random import randint
-
+#menu
 print('=================================')
 print('Rock Paper Scissors Lizard Spock')
 print('=================================')
@@ -8,10 +9,13 @@ print('2) ✋')
 print('3) ✌️')
 print('4) 🦎')
 print('5) 🖖')
-
+#options List
 options=['✊', '✋','✌️','🦎','🖖']
+#Loop
 while True:
+    #Loop
     while True:
+        #handle user input errors
         try:
             player=int(input('Pick a number: '))
             if 1 <= player <= 5:
@@ -20,15 +24,15 @@ while True:
                 print('¡Invalid number! Please choose between 1, 2, 3, 4 or 5.')
         except ValueError:
             print('Invalid entry! Please enter a number.')
-
+    #randint() function of the "random" module
     computer=randint(0,4)
-
+    #variables
     player = options[player-1]
     computer = options[computer]
-
+    #Print variables
     print('You chose: ', player)
     print('CPU chose: ', computer)
-
+    #if-else control flow
     if player == computer:
         print('It is a tie!')
     elif (player == '✌️' and computer == '✋'):
@@ -51,11 +55,9 @@ while True:
         print('The player won!')
     elif (player == '✊' and computer == '✌️'): 
         print('The player won!')
-    
-        
     else:
         print('The cpu won!')
-
+    #loop control
     quest=input('Pres *Y* to continue or *N* for exit: ') 
     quest= quest.upper()
     if quest == 'N':
